f559aca9fe47e6bf122e88d798d97f181b2aa091
Forrest Wilson-Jennings <forrest@Forrests-MacBook-Pro.local>
2020-09-17 21:09:50 +1200

    Adding create method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { title } from 'process';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,5 +12,13 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks()
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string
+  ) {
+      return this.tasksService.createTask(title, description)
   }
 }
