dc3e7e8e867258a8b7bd75c13b2e68a629510bf7
Forrest Wilson-Jennings <forrest@Forrests-MacBook-Pro.local>
2020-09-19 20:19:04 +1200

    Creating custom decorator for getting user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,9 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
