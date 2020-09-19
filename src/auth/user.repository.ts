574e5f13d1062012864da9fbdb461e1ccf9cef0e
Forrest Wilson-Jennings <forrest@Forrests-MacBook-Pro.local>
2020-09-19 19:42:54 +1200

    Added signin method

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -15,7 +15,7 @@ export class UserRepository extends Repository<User> {
     const user = new User();
 
     user.username = username;
-    user.salt = await bcrypt.genSalt();;
+    user.salt = await bcrypt.genSalt();
     user.password = await this.hashPassword(password, user.salt);
 
     try {
@@ -27,6 +27,19 @@ export class UserRepository extends Repository<User> {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
     }
   }
 
